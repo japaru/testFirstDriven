7e6d16208b100b01e34da925cf45c533d53b3b0f
Javier Antonio Valdez PALOMINO <javier.antonio.valdez.palomino@ericsson.com>
2019-05-27 09:06:46 +0200

    removing cardStack Inheritance from Stock

--- src/main/java/klondike/models/Stock.java
+++ src/main/java/klondike/models/Stock.java
@@ -4,23 +4,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Stock extends CardStack {
+public class Stock {
 
+	private CardStack cardStack;
+	
     public Stock() {
-        super();
+		this.cardStack = new CardStack();
+		
         for (Suit suit : Suit.values()) {
             for (Number number : Number.values()) {
-                this.cards.add(new Card(suit, number));
+                this.cardStack.cards.add(new Card(suit, number));
             }
         }
-        Collections.shuffle(this.cards);
+        Collections.shuffle(this.cardStack.cards);
     }
 
     public List<Card> takeTop(int quantity) {
-        assert 0 < quantity && quantity <= this.cards.size();
-        List<Card> cardsToReturn = new ArrayList<Card>(this.cards.subList(0, quantity));
-        this.cards.removeAll(cardsToReturn);
+        assert 0 < quantity && quantity <= this.cardStack.cards.size();
+        List<Card> cardsToReturn = new ArrayList<Card>(this.cardStack.cards.subList(0, quantity));
+        this.cardStack.cards.removeAll(cardsToReturn);
         return cardsToReturn;
     }
 
+    public boolean empty() {
+        return this.cardStack.empty();
+    }
+
+    public Card peek() {
+        return this.cardStack.peek();
+    }
+
+    public Card pop() {
+        return this.cardStack.pop();
+    }
+
+    public void push(Card card) {
+        this.cardStack.push(card);
+    }
 }
