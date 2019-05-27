8493eefda401957fb7cb652255974346f087bf9a
Javier Antonio Valdez PALOMINO <javier.antonio.valdez.palomino@ericsson.com>
2019-05-27 16:51:28 +0200

    cardStack not needed removing it from Waste

--- src/main/java/klondike/models/Waste.java
+++ src/main/java/klondike/models/Waste.java
@@ -1,26 +1,28 @@
 package klondike.models;
 
+import java.util.Stack;
+
 public class Waste {
-	private CardStack cardStack;
+    private Stack<Card> cards;
 	
 	public Waste() {
-		this.cardStack = new CardStack();
+		this.cards = new Stack<Card>();
 	}
 	
     public boolean empty() {
-        return this.cardStack.empty();
+        return this.cards.empty();
     }
 
     public Card peek() {
-        return this.cardStack.peek();
+        return this.cards.peek();
     }
 
     public Card pop() {
-        return this.cardStack.pop();
+        return this.cards.pop();
     }
 
     public void push(Card card) {
-        this.cardStack.push(card);
+        this.cards.push(card);
     }
 
 }
